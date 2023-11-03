1d03345d4942796749e617617a7e4a46c7f674f2
Ramiro Silva <134079069+RamiroS26@users.noreply.github.com>
2023-11-03 20:39:14 +0000

    guia 1 hasta el punto 9

---
+++ entrega/guia1.py
@@ -0,0 +1,155 @@
+# 1
+
+#a
+
+#print("Las máquinas me sorprenden con mucha frecuencia.")
+
+#b
+
+#print()
+
+#c
+
+#print("Hola Mundo")
+
+#d
+
+#print("23", 23)        El "23" se muestra como un string.
+
+#e
+
+#print("Una computadora puede ser llamada ""inteligente"" si logra engañar a una persona haciéndole creer que es un humano.” Las comillas que encierran a la palabra inteligente deben imprimirse.")
+
+#f
+
+#print("Ramiro", "Silva", "20")
+
+#g
+
+#print("Ramiro", "Silva", "20", sep="_")
+
+#h
+
+#print("Calle\t", "Numero\t", "Codigo")
+
+#i
+
+#print("Calle\n", "Numero\n", "Codigo")
+
+#j
+
+#var="Feliz\n\tPrimavera\n\t\t2023"
+#print(var)
+
+#k
+
+#print("Solo podemos ver poco del futuro,", end=" ")
+#print("pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+
+#l
+
+#print("    *")
+#print("   * *")
+#print("  * * *")
+#print(" * * * *")
+#print("*** *** ***")
+#print("   * *")
+#print("   * *")
+#print("  *****")
+
+#3 y 4
+
+#a
+
+#nombre     String
+
+#b
+
+#apellido       String
+
+#c
+
+#edad       Int
+
+
+#d
+
+#altura     Int
+
+#e
+
+#num_vuelo      Int
+
+#f
+
+#temp_ambiente      Int
+
+#g
+
+#salario_mensual        Float
+
+#h
+
+#termino_juego      Boolean
+
+#i
+
+#es_par     Boolean
+
+#5
+
+#a
+
+#nombre = input("Ingrese su nombre: ")
+#apellido = input("Ingrese su apellido: ")
+#edad = input("Ingrese su edad: ")
+#print(f"{nombre} {apellido}, ser creativo a los {edad} años.")
+
+#b
+
+#nombre = input("Ingrese su nombre: ")
+#apellido = input("Ingrese su apellido: ")
+#edad = input("Ingrese su edad: ")
+
+#mensaje_creativo = f"{nombre} {apellido}, ser creativo a los {edad} años."
+#input(mensaje_creativo)
+
+#6
+
+#numero1 = int(input("Ingrese el primer número entero: "))
+#numero2 = int(input("Ingrese el segundo número entero: "))
+#suma = numero1 + numero2
+#resta = numero1 - numero2
+#producto = numero1 * numero2
+#potencia = numero1 ** numero2
+#resto = numero1 % numero2
+
+#7
+
+#numero_entero = int(input("Ingrese un número entero: "))
+#numero_float = float(input("Ingrese un número decimal (float): "))
+#suma = numero_entero + numero_float
+#resta = numero_entero - numero_float
+#producto = numero_entero * numero_float
+#potencia = numero_entero ** numero_float
+#resto = numero_entero % numero_float
+
+#8
+
+#base_rectangulo = float(input("Ingrese la base del rectángulo: "))
+#altura_rectangulo = float(input("Ingrese la altura del rectángulo:"))
+
+#perimetro_rectangulo = 2 * (base_rectangulo + altura_rectangulo)
+#area_rectangulo = base_rectangulo * altura_rectangulo
+#print(f"Rectángulo - Perímetro: {perimetro_rectangulo}, Área: {area_rectangulo}")
+
+#radio_circunferencia = float(input("Ingrese el radio de la circunferencia: "))
+
+#pi = 3.14
+
+#perimetro_circunferencia = 2 * pi * radio_circunferencia
+#area_circunferencia = pi * radio_circunferencia ** 2
+#print(f"Circunferencia - Perímetro: {perimetro_circunferencia}, Área: {area_circunferencia}")
+
+#9
+
