46dd465703e7a2fd8bc082b6aa4250c905c90ef2
Ramiro Silva <134079069+RamiroS26@users.noreply.github.com>
2023-11-08 17:00:38 +0000

    guia 3 terminada

--- entrega/guia3.py
+++ entrega/guia3.py
@@ -29,8 +29,8 @@
 
 #5
 
-#for k in range(1,8):
-#    print("#"*k)
+#for i in range(1,8):
+#    print("#"*i)
 
 #6
 
@@ -43,5 +43,72 @@
 
 #nombre = input("Ingrese su nombre de usuario: ")
 #numero = int(input("Ingrese un numero entero: "))
-#for _ in range(numero):
+#for i in range(numero):
 #    print(nombre)
+
+#8
+
+#num = int(input("Ingrese un numero mayor a 3: "))
+#for i in range(1,num+1,2):
+#    print(i)
+
+#9
+
+#for i in range(0,12):
+#    print(f"{i} x {i} = {i*i}")
+
+#10
+
+#for i in range(7):
+#    for j in range(i, 7):
+#        print(f"{i} : {j}")
+
+#11
+
+#num = int(input("Ingrese un numero entero: "))
+#for i in range(1, num + 1, 2):
+#    for j in range(i, 0, -2):
+#        print(j, end=' ')
+#    print()
+
+#12
+
+#n = int(input("Ingrese un numero natural: "))
+#suma = 0
+#suma_str =""
+#for i in range(1,n+1):
+#    suma += i
+#    suma_str += str(i)
+#    if i<n:
+#        suma_str += (" + ")
+#print(f"La suma de los numeros desde 1 hasta {n} es: {suma_str} = {suma}")
+
+#13
+
+#n = int(input("Ingrese un numero natural: "))
+#suma = 0
+#for i in range(2,2*n+1,2):
+#    suma += i
+#print(f"La suma de los primeros {n} numeros pares es: {suma}")
+
+#14
+
+#num1 = int(input("Ingrese un numero: "))
+#num2 = int(input("Ingrese otro numero: "))
+#for i in range(num1+1,num2):
+#    if i%2==0:
+#        print(i)
+
+#15
+
+#numero = int(input("Ingrese un número entero: "))
+#if numero>1:
+#    for i in range(2, numero):
+#        if (numero % i) == 0:
+#            print(f"{numero} no es primo")
+#            break
+#    else:
+#        print(f"{numero} es primo")
+#else:
+#    print(f"{numero} no es primo")
+
