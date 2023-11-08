9465b03149a5ab27513305c472d2619765ee132f
Ramiro Silva <134079069+RamiroS26@users.noreply.github.com>
2023-11-08 02:12:14 +0000

    guia 3 hasta punto 7

--- entrega/guia3.py
+++ entrega/guia3.py
@@ -7,6 +7,41 @@
 
 #2
 
-for num in range (101):
-    print(num, end=' ')
+#for num in range (101):
+#    print(num, end=' ')
 
+#3
+
+#num = 10
+#while num >= 0:
+#    print(num, end=' ')
+#    num -= 1
+
+#for num in range(10, -1, -1):
+#    print(num, end=' ')
+
+#4
+
+#num1 = int(input("Ingrese un numero entero: "))
+#num2 = int(input("Ingrese otro numero entero: "))
+#for num in range(num1+1,num2):
+#    print(num, end=' ')
+
+#5
+
+#for k in range(1,8):
+#    print("#"*k)
+
+#6
+
+#for i in range(8):
+#    for j in range(8):
+#        print("#", end='')
+#    print("")
+
+#7
+
+#nombre = input("Ingrese su nombre de usuario: ")
+#numero = int(input("Ingrese un numero entero: "))
+#for _ in range(numero):
+#    print(nombre)
